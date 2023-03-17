8ba17e2f7aaeab12fee311aa68f0decc4082f359
ryankimutai <ryankimutai505@gmail.com>
2023-03-17 11:12:17 -0700

    Positive anything is better than negative nothing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("size of char: %lu byte(s)", size_t of (char));
+	printf("size of int: %lu byte(s)", size of (int));
+	printf("size of long int: %lu byte(s)", size of (long int));
+	printf("size of long long int: %lu byte(s)", size of (long long int)); 
+	printf("size of float: %lu byte(s)", size of (float));
+	return (0);
+}
